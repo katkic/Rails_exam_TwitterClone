fe9a75e6daf93d72779629d32146d2f10ba4a8e5
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-09-05 23:47:13 +0900

    新規投稿機能を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,17 +8,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
