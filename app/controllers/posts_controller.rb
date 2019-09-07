02694d7e07a39e5f043e4b765078b44b3dfaa66a
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-09-07 22:44:43 +0900

    showアクションの記述を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
