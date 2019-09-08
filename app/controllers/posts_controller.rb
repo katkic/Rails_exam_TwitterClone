33bad3164ab23c0a337af79d723502f6d0487bba
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-09-08 20:23:40 +0900

    showアクションを削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order(created_at: :desc)
-  end
-
-  def show
   end
 
   def new
