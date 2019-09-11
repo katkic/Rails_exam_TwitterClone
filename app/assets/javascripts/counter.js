8df6e79624188ec4d1e8717f27f75a033262a649
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-09-11 18:51:30 +0900

    文字数カウンターのロジックを変更

--- app/assets/javascripts/counter.js
+++ app/assets/javascripts/counter.js
@@ -1,13 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
   const content = document.querySelector('#post_content');
   const countText = document.querySelector('#show-count-text')
+  countText.innerText = content.value.replace(/\n/g, '\r\n').length;
 
-  if (countText != null) {
-    countText.innerText = content.value.replace(/\n/g, '\r\n').length;
-
-    content.addEventListener('input', function() {
-      let count = this.value.replace(/\n/g, '\r\n').length;
-      countText.innerText = count;
-    }, false);
-  }
+  content.addEventListener('input', function() {
+    let count = this.value.replace(/\n/g, '\r\n').length;
+    countText.innerText = count;
+  }, false);
 }, false);
